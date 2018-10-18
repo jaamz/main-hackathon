23feb807b9cbc304a3d29fca20cfb78b78b19cb8
t-dinh <tdinh104@gmail.com>
2018-10-18 15:47:39 -0700

    table set up

--- Client/src/Components/JobPage.js
+++ Client/src/Components/JobPage.js
@@ -11,15 +11,27 @@ class JobPage extends Component {
         return ( 
             <div className="row" id="container">
                 <div id="threadBox">
-                    <h1>Thread</h1>
+                    <h1>Jobs</h1>
+                    <table className= "table">
+                        <thead>
+
+                        </thead>
+                        <tbody>
+                            <tr>this is going to map out threads</tr>
+                            <tr>t row 2</tr>
+                        </tbody>
+                    
+                    </table>
                     {/* map out all the threads according to the submissions. 
                     each thread should be clickable */}
                     {/* <div className= 'threadBox'></div> */}
                 </div>
                 <div className="col-md-4" id="formBox">
-
+                    <div>
+                    <h1>Create a thread</h1>
                     <FormSubmit 
                     addToThread = {this.props.addToThread}/>
+                    </div>
                 </div>
             </div>
 
