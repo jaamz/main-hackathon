2344addfea8e7f0c7b75b1e8a9327b2b39815fa4
James Park <jamestkd918@gmail.com>
2018-10-18 14:34:16 -0700

    #got rid of bloatware #created skeleton for form submit #created jobpage skeleton

---
+++ Client/src/Components/FormSubmit.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class FormSubmit extends Component {
+    state = {
+        title: '',
+        body: '',
+    }
+
+    onTitleChange = e => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    onBodyChange = e => {
+        this.setState({
+            body:e.target.value
+        })
+    }
+
+    onClick = e => {
+        e.preventDefault();
+        this.setState({
+            title:'',
+            body:''
+        })
+    }
+
+
+
+
+
+
+    render() {
+
+        return (
+            <div>
+                <form
+                onSubmit={this.onClick}>
+                    <div>
+                        <input
+                        value={this.state.title}
+                        onChange={this.onTitleChange}
+                        />
+                        </div>
+                        <div>
+                            <textarea 
+                            value={this.state.body}
+                            onChange={this.onBodyChange}
+                            />
+                        </div>
+                        <div>
+                            <button 
+                            className='btn btn-primary'
+                            />
+                    </div>
+                </form>
+
+
+            </div>
+        );
+    }
+}
+
+export default FormSubmit;
---
+++ Client/src/Components/JobPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import FormSubmit from './FormSubmit';
+
+class JobPage extends Component {
+    state = {
+        threads:[],
+
+    }
+
+    addToThread = submittedThread => {
+        let newThread = [...this.state.threads, submittedThread];
+        this.setState({
+            threads: newThread
+        })
+    }
+
+    render() {
+
+
+
+        return ( 
+            <div>
+                <div>
+                    <h1>Thread</h1>
+                </div>
+                <div>
+                    <FormSubmit />
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default JobPage;
