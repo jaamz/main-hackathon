1e44d1efb9c5c979094241e39f09c9c8f356cf98
Patrick Truong <pattruong@gmail.com>
2018-10-19 10:43:57 -0700

    uploaded without security

--- Server/Controllers/AppUserController.cs
+++ Server/Controllers/AppUserController.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace Server
 {
-    [Route("api/appusers")]
+    [Route("api/users")]
     [ApiController]
     public class AppUserController2 : Controller
     {
@@ -32,53 +32,53 @@ namespace Server
 
             return Ok(_context.appuser.ToList());
         }
-        [HttpPost]
-        public ActionResult<AppUser> Register([FromBody] AppUser user)
-        {
+        // [HttpPost]
+        // public ActionResult<AppUser> Register([FromBody] AppUser user)
+        // {
 
-            user.password = BCrypt.Net.BCrypt.HashPassword(user.password);
+        //     user.password = BCrypt.Net.BCrypt.HashPassword(user.password);
 
-            _context.appuser.Add(user);
-            _context.SaveChanges();
+        //     _context.appuser.Add(user);
+        //     _context.SaveChanges();
 
-            return Ok(user);
-        }
+        //     return Ok(user);
+        // }
 
-        [HttpPost("login")]
-        public ActionResult Login([FromBody] AppUser user)
-        {
-            foreach (AppUser u in _context.appuser)
-            {
-                if (u.username == user.username)
-                {
-                    bool verified = BCrypt.Net.BCrypt.Verify(user.password, u.password);
-                    if (verified)
-                    {
-                        string token = BuildToken(u);
-                        return Ok(token);
-                    }
-                    else
-                    {
-                        return NotFound();
-                    }
-                }
-            }
+        // [HttpPost("login")]
+        // public ActionResult Login([FromBody] AppUser user)
+        // {
+        //     foreach (AppUser u in _context.appuser)
+        //     {
+        //         if (u.username == user.username)
+        //         {
+        //             bool verified = BCrypt.Net.BCrypt.Verify(user.password, u.password);
+        //             if (verified)
+        //             {
+        //                 string token = BuildToken(u);
+        //                 return Ok(token);
+        //             }
+        //             else
+        //             {
+        //                 return NotFound();
+        //             }
+        //         }
+        //     }
 
-            return NotFound();
-        }
+        //     return NotFound();
+        // }
 
-        public string BuildToken(AppUser user)
-        {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretsuperSecretsuperSecret"));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        // public string BuildToken(AppUser user)
+        // {
+        //     var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretsuperSecretsuperSecret"));
+        //     var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(
-                "localhost", 
-                "localhost", 
-                expires: DateTime.Now.AddSeconds(15),
-                signingCredentials: creds);
+        //     var token = new JwtSecurityToken(
+        //         "localhost", 
+        //         "localhost", 
+        //         expires: DateTime.Now.AddSeconds(15),
+        //         signingCredentials: creds);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
+        //     return new JwtSecurityTokenHandler().WriteToken(token);
+        // }
     }
 }
